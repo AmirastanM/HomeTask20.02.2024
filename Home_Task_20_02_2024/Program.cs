e72c933ea6d6f753095468a66cc0ed33a61d4e01
AmirastanM <amirastanshm@code.edu.az>
2024-02-21 01:43:52 +0400

    added home task

---
+++ Home_Task_20_02_2024/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+// Method yazirsiz, Method stringden ibaret array ve char qebul edir. Method arrayin icerisinde gelen chardan ne qederdirse onlarin sayini geriye qaytarsin.
+
+
+
+using Microsoft.VisualBasic;
+using System;
+
+int SumOfCounts(string[] array, char edeler)
+{
+    int count = 0;
+    foreach (string str in array)
+    {
+        count += str.Count(m => m == edeler);
+    }
+    return count;
+}
+
+int result = SumOfCounts(string "");
+
+SumOfCounts("salam") ;
